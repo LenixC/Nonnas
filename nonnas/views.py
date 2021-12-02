efbe88390e18fb3d76919d002a1f4ba7d5529b8f
Lenix <lenix.carter@emory.edu>
2021-12-01 23:14:03 -0500

    Added upvoting

--- nonnas/views.py
+++ nonnas/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 def homepage(request):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, pk=request.POST['post'])
+        post.votes += 1
+        post.save()
     post_list = Post.objects.all()
     context = {'post_list': post_list}
     return render(request, 'nonnas/homepage.html', context)
